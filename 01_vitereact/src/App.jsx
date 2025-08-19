d9b58b58f91f45ae4285354b3757c3812e236303
Sabbir486 <australiasabbir355@gmail.com>
2025-08-19 15:55:28 +0600

    React

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,10 +1,9 @@
+import Chai from "./chai"
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Chai/>
   )
 }
 
---
+++ 01_vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h3>Sabbir Ahmed is a good boy</h3>
+    )
+}
+
+export default Chai
